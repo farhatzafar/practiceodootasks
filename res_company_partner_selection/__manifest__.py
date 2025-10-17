0388cea735eb0f2e5187306e616a1182dca7903f
Farhat Zafar <farhatzafar63@gmail.com>
2025-10-17 16:39:15 -0400

    36803

---
+++ res_company_partner_selection/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "Res Company Partner Selection",
+    "summary": "Allows partner selection for res company",
+    "version": "18.0.1.0.0",
+    "depends": ['base'],
+    "data": ['views/res_company_views.xml'],
+    "installable": True,
+}

f8c55cf28d45dad2bd4b34624320168d3b722805
Farhat Zafar <farhatzafar63@gmail.com>
2025-10-24 15:16:33 -0400

    37020

---
+++ sale_subscription_invoice_document/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Sale Subscription Invoice Document",
+    "summary": "A custom invoice pdf for subscription module",
+    "version": "18.0.1.0.0",
+    "depends": [
+        "sale_subscription",
+        "rccaq_sale_subscription",
+        "account"
+    ],
+    "data": ['reports/report_invoice.xml'],
+    "installable": True,
+}

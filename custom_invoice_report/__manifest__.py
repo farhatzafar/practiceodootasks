f9f486f240a500a79cfdacbd2940e84176fa6c47
Farhat Zafar <farhatzafar63@gmail.com>
2025-12-04 14:57:49 -0500

    37920

---
+++ custom_invoice_report/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "Custom Invoice Report",
+    "version": "18.0.1.0.0",
+    "summary": "Removes the due date from invoice pdf",
+    "depends": ['account'],
+    "data": ['report/invoice_report.xml'],
+    "installable": True
+}
